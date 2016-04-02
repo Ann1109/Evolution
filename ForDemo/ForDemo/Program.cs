b093e1496cab2215b807facb62da7b67ca6ac35a
ann <bengy@bk.ru>
2016-04-02 20:34:09 +0300

    ForDemo & RandomDemo

---
+++ ForDemo/ForDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace вывести_числа_от_1_до_1000
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                Console.Write(i + " ");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomDemo/RandomDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RandomDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+            Console.WriteLine(value); 
+            Console.ReadKey();
+        }
+    }
+}
+
