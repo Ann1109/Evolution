acb26289a0b48bcd847ad7340a91f390a9a8802d
ann <bengy@bk.ru>
2016-04-09 20:16:47 +0300

    проект сцена кубик вращается

---
+++ CUBE 09.04.16/Assets/RotateCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+    Quaternion originRotation;
+    float angle;
+
+	// Use this for initialization
+	void Start () {
+        originRotation = transform.rotation;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        angle++;
+        Quaternion rotationY = Quaternion.AngleAxis(angle, Vector3.up);
+        Quaternion rotationX = Quaternion.AngleAxis(angle, Vector3.right);
+
+        transform.rotation = rotationY * rotationX;
+    }
+}
